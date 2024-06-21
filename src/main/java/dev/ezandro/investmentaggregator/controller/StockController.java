86c78160db8e7a5b6c22cf8e9581bbbe57f78623
ezbueno <ezandrobueno@yahoo.com.br>
2024-06-21 01:12:04 -0300

    Create StockController

--- src/main/java/dev/ezandro/investmentaggregator/controller/StockController.java
+++ src/main/java/dev/ezandro/investmentaggregator/controller/StockController.java
@@ -1,2 +1,25 @@
-package dev.ezandro.investmentaggregator.controller;public class StockController {
+package dev.ezandro.investmentaggregator.controller;
+
+import dev.ezandro.investmentaggregator.dto.CreateStockDTO;
+import dev.ezandro.investmentaggregator.service.StockService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1/stocks")
+public class StockController {
+    private final StockService stockService;
+
+    public StockController(StockService stockService) {
+        this.stockService = stockService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createStock(@RequestBody CreateStockDTO createStockDTO) {
+        this.stockService.createStock(createStockDTO);
+        return ResponseEntity.ok().build();
+    }
 }
