aabb08bdd17192b3814d014b9082c6865afd2e2a
ezbueno <ezandrobueno@yahoo.com.br>
2024-06-21 01:12:26 -0300

    Create StockService

--- src/main/java/dev/ezandro/investmentaggregator/service/StockService.java
+++ src/main/java/dev/ezandro/investmentaggregator/service/StockService.java
@@ -1,2 +1,20 @@
-package dev.ezandro.investmentaggregator.service;public class StockService {
+package dev.ezandro.investmentaggregator.service;
+
+import dev.ezandro.investmentaggregator.dto.CreateStockDTO;
+import dev.ezandro.investmentaggregator.entity.Stock;
+import dev.ezandro.investmentaggregator.repository.StockRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockService {
+    private final StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    public void createStock(CreateStockDTO createStockDTO) {
+        var stock = new Stock(createStockDTO.stockId(), createStockDTO.description());
+        this.stockRepository.save(stock);
+    }
 }
