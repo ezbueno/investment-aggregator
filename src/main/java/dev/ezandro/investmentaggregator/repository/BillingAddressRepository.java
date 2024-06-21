2bfc84e453cb708e9681be501ed4cad4152a7916
ezbueno <ezandrobueno@yahoo.com.br>
2024-06-20 23:06:41 -0300

    Add BillingAddressRepository

--- src/main/java/dev/ezandro/investmentaggregator/repository/BillingAddressRepository.java
+++ src/main/java/dev/ezandro/investmentaggregator/repository/BillingAddressRepository.java
@@ -1,9 +1,9 @@
 package dev.ezandro.investmentaggregator.repository;
 
-import dev.ezandro.investmentaggregator.entity.Account;
+import dev.ezandro.investmentaggregator.entity.BillingAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface AccountRepository extends JpaRepository<Account, UUID> {
+public interface BillingAddressRepository extends JpaRepository<BillingAddress, UUID> {
 }
