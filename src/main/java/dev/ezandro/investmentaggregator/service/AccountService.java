2945fe468af55577a335441910f6372b87d74c90
ezbueno <ezandrobueno@yahoo.com.br>
2024-06-27 17:23:45 -0300

    Update AccountService

--- src/main/java/dev/ezandro/investmentaggregator/service/AccountService.java
+++ src/main/java/dev/ezandro/investmentaggregator/service/AccountService.java
@@ -1,5 +1,6 @@
 package dev.ezandro.investmentaggregator.service;
 
+import dev.ezandro.investmentaggregator.client.BrapiClient;
 import dev.ezandro.investmentaggregator.dto.AccountStockResponseDTO;
 import dev.ezandro.investmentaggregator.dto.AssociateAccountStockDTO;
 import dev.ezandro.investmentaggregator.entity.AccountStock;
@@ -7,6 +8,7 @@ import dev.ezandro.investmentaggregator.entity.AccountStockId;
 import dev.ezandro.investmentaggregator.repository.AccountRepository;
 import dev.ezandro.investmentaggregator.repository.AccountStockRepository;
 import dev.ezandro.investmentaggregator.repository.StockRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,14 +18,22 @@ import java.util.UUID;
 
 @Service
 public class AccountService {
+    @Value("#{environment.token}")
+    private String token;
+
     private final AccountRepository accountRepository;
     private final AccountStockRepository accountStockRepository;
     private final StockRepository stockRepository;
+    private final BrapiClient brapiClient;
 
-    public AccountService(AccountRepository accountRepository, AccountStockRepository accountStockRepository, StockRepository stockRepository) {
+    public AccountService(AccountRepository accountRepository,
+                          AccountStockRepository accountStockRepository,
+                          StockRepository stockRepository,
+                          BrapiClient brapiClient) {
         this.accountRepository = accountRepository;
         this.accountStockRepository = accountStockRepository;
         this.stockRepository = stockRepository;
+        this.brapiClient = brapiClient;
     }
 
     public void associateStock(String accountId, AssociateAccountStockDTO associateAccountStockDTO) {
@@ -45,7 +55,16 @@ public class AccountService {
 
         return account.getAccountStocks()
                 .stream()
-                .map(accountStock -> new AccountStockResponseDTO(accountStock.getStock().getStockId(), accountStock.getQuantity(), 0.0))
+                .map(accountStock -> new AccountStockResponseDTO(
+                        accountStock.getStock().getStockId(),
+                        accountStock.getQuantity(),
+                        this.getTotal(accountStock.getQuantity(), accountStock.getStock().getStockId())))
                 .toList();
+    }
+
+    private double getTotal(Integer quantity, String stockId) {
+        var response = this.brapiClient.getQuote(this.token, stockId);
+        var price = response.results().getFirst().regularMarketPrice();
+        return quantity * price;
     }
 }
